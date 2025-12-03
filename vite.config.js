9fa6819aba1a2e386889abffbd21473febe7dee1
dvasilchuk <nuvolanam@gmail.com>
2025-12-03 18:39:03 +0300

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/react-interview-prep/', // Замените на имя вашего репозитория
   server: {
     port: 3000,
     open: true
   }
 })
-
